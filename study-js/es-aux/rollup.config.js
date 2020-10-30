59ecfad76fc7cf29af68a53db22be5cc68e5e8a8
yupeng.bai <yupeng.bai@ucarinc.com>
2020-10-30 16:09:27 +0800

    feat✨ study

---
+++ study-js/es-aux/rollup.config.js
@@ -0,0 +1,106 @@
+import nodeResolve from 'rollup-plugin-node-resolve'
+import banner from 'rollup-plugin-banner'
+
+import pkg from './package.json'
+import commonjs from 'rollup-plugin-commonjs'
+import buble from 'rollup-plugin-buble'
+import { terser } from 'rollup-plugin-terser'
+import typescript from 'rollup-plugin-typescript'
+import json from 'rollup-plugin-json';
+
+
+const bannerTemplate =
+  `es-aux.js v${pkg.version}
+(c) 2019-${new Date().getFullYear()} staven
+https://github.com/staven630/es-aux
+Released under the MIT License.
+`
+
+const terserPlugin = terser({
+  output: {
+    ascii_only: true // 仅输出ascii字符
+  },
+  compress: {
+    pure_funcs: ['console.log'] // 去掉console.log函数
+  }
+})
+
+const plugins = [
+  typescript({
+    exclude: ['node_modules/**', '**/__tests__/**'],
+    typescript: require('typescript')
+  }),
+  json(),
+  nodeResolve({
+    jsnext: true,
+    main: true
+  }),
+  commonjs({ extensions: ['.js', '.ts'] }),
+  buble({
+    objectAssign: 'Object.assign',
+    exclude: ['node_modules/**']
+  }),
+
+]
+
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {})
+]
+
+
+export default [
+  {
+    input: 'src/main.ts',
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      indent: false,
+    },
+    external,
+    plugins: [
+      ...plugins,
+      terserPlugin,
+      banner(bannerTemplate)
+    ]
+  },
+  {
+    input: 'src/main.ts',
+    output: {
+      file: pkg.module,
+      format: 'es',
+      indent: false,
+    },
+    external,
+    plugins: [
+      ...plugins,
+      terserPlugin,
+      banner(bannerTemplate)
+    ]
+  },
+  {
+    input: 'src/main.ts',
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+      indent: false,
+      name: 'Aux',
+    },
+    external,
+    plugins: [
+      ...plugins,
+      terserPlugin,
+      banner(bannerTemplate)
+    ]
+  },
+  {
+    input: 'src/main.ts',
+    output: {
+      file: pkg.module.replace('.esm', ''),
+      format: 'es',
+      indent: false,
+    },
+    external,
+    plugins
+  },
+]
